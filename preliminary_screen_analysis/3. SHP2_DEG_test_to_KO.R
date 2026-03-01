6a06d7ba246b4d6eeee581438bf49573642ef306
rmg2213 <97689186+rmg2213@users.noreply.github.com>
2026-03-01 10:25:11 -0500

    Update with new analysis

--- preliminary_screen_analysis/3. SHP2_DEG_test_to_KO.R
+++ preliminary_screen_analysis/3. SHP2_DEG_test_to_KO.R
@@ -415,3 +415,308 @@ for (new_element in names(gene_sets)) {
 }
 
 saveRDS(gene_sets_final, "SHP2_mut_DEGdown_intersection.RDS")
+
+# ==============================================================================
+# Analyzing intersections of DEGs between SHP2 mutants
+# ==============================================================================
+
+# ================================================================================
+# Overlap of DEGs for mock or WT vs DMSO
+# ================================================================================
+
+treatment_diff_test_SHP2 <- treatment_diff_test %>%
+  filter(grepl(pattern = "SHP2_mut", term)) %>%
+  filter(abs(normalized_effect) > 0.25 & q_value < 0.05) %>%
+  mutate(direction = case_when(
+    normalized_effect >= 0 ~ "Upregulated",
+    TRUE ~ "Downregulated"
+  )) %>%
+  filter(SHP2_mut %in% c("WT", "Q510E")) %>%
+  filter(direction == "Upregulated") %>%
+  filter(timepoint == "24hr") %>%
+  mutate(SHP2_mut = case_when(
+    SHP2_mut == "Q510E" ~ "Mock Transfected",
+    TRUE ~ SHP2_mut
+  ))
+
+upset.list <- list()
+for (mut in unique(treatment_diff_test_SHP2$SHP2_mut) %>% sort()) {
+  upset.list[[mut]] <- treatment_diff_test_SHP2 %>% 
+    filter(SHP2_mut == mut & direction == "Upregulated") %>% 
+    pull(gene_short_name)
+}
+
+intersection <- ComplexHeatmap::make_comb_mat(upset.list,
+                                              mode = "distinct")
+
+intersection <- intersection[comb_size(intersection) >= 4]
+gene_sets <- sapply(comb_name(intersection), function(nm) extract_comb(intersection, nm))
+test <- set_name(intersection)
+test_names <- data.frame(x = names(gene_sets)) %>%
+  separate(x, into = as.character(c(1:11)), sep = c(1,2,3,4,5,6,7,8,9,10,11)) %>%
+  mutate(`1` = ifelse(`1` == 1, test[1],""),
+         `2` = ifelse(`2` == 1, test[2],""),
+         `3` = ifelse(`3` == 1, test[3],""),
+         `4` = ifelse(`4` == 1, test[4],""),
+         `5` = ifelse(`5` == 1, test[5],""),
+         `6` = ifelse(`6` == 1, test[6],""),
+         `7` = ifelse(`7` == 1, test[7],""),
+         `8` = ifelse(`8` == 1, test[8],""),
+         `9` = ifelse(`9` == 1, test[9],""),
+         `10` = ifelse(`10` == 1, test[10],""),
+         `11` = ifelse(`11` == 1, test[11],"")) %>%
+  unite(col = "name", sep = " ", 1:11)
+
+clean_mutations <- function(x) {
+  x <- gsub("^\\s+|\\s+$", "", x)     # Remove leading and trailing spaces
+  x <- gsub("\\s+", " ", x)           # Replace multiple spaces with a single space
+  x <- gsub(" ", "_", x)              # Replace spaces with underscores
+  return(x)
+}
+
+# Apply the function to the name column
+name <- sapply(test_names, clean_mutations)
+
+# Convert back to data frame if necessary
+names(gene_sets) <- c("Mock Transfected_WT", "Mock Transfected", "WT")
+
+# Grab DEG test information for each element in list
+gene_sets_final <- list()
+for (new_element in names(gene_sets)) {
+  new_element_split <- stringr::str_split_1(string = new_element, pattern = "_")
+  gene_set_df_temp <- treatment_diff_test_SHP2 %>%
+    filter(gene_short_name %in% gene_sets[[new_element]]) %>%
+    filter(SHP2_mut %in% new_element_split) %>%
+    select(-id, -status, -estimate, -std_err, -test_val, -p_value, -model_component) %>%
+    as.data.frame()
+  gene_sets_final[[new_element]] <- gene_set_df_temp
+}
+
+test <- gene_sets_final[["Mock Transfected_WT"]] %>%
+  select(gene_short_name, normalized_effect, SHP2_mut, timepoint, direction) %>%
+  pivot_wider(id_cols = c(gene_short_name,
+                          timepoint,
+                          direction),
+              names_from = SHP2_mut,
+              values_from = normalized_effect)
+
+readr::write_csv(x = test,
+                 file = "SHP2_WT_mock_upregulated_intersection.csv",
+                 col_names = T)
+
+gene_universe <- treatment_diff_test %>%
+  filter(grepl(pattern = "SHP2_mut", term)) %>%
+  filter(abs(normalized_effect) >= 0.25 & q_value <= 0.05) %>%
+  select(gene_short_name) %>%
+  distinct(gene_short_name) %>%
+  pull()
+
+source("GSA_helper_functions.r")
+source("loadGSCSafe.R")
+
+hallmarksGSC <- loadGSCSafe(file = "h.all.v6.0.symbols.gmt",
+                            type = "gmt")
+
+hallmarksGSC_length <- data.frame("gene_set" = names(hallmarksGSC[["gsc"]])) %>%
+  tibble::rownames_to_column("row")
+
+GSAhyper_df_final <- data.frame()
+contingency_tab_list <- list()
+for (temp_name in c("WT", "Mock Transfected_WT", "Mock Transfected")) {
+  gene_test <- gene_sets_final[[temp_name]] %>%
+    select(gene_short_name) %>% 
+    distinct(gene_short_name) %>% 
+    pull()
+  
+  test <- piano::runGSAhyper(genes = gene_test, 
+                             gsc = hallmarksGSC, 
+                             adjMethod = "BH", 
+                             universe = gene_universe)
+  
+  GSAhyper_df <- as.data.frame(test$pvalues) %>%
+    tibble::rownames_to_column()
+  colnames(GSAhyper_df) <- c("gene_set","p_value")
+  
+  for (i in GSAhyper_df$gene_set) {
+    
+    hallmarkGSC_index <- hallmarksGSC_length %>%
+      mutate(row = as.double(row)) %>%
+      filter(gene_set == i) %>%
+      pull(row)
+    
+    contingency_tab_list[[paste0(temp_name)]][[i]] <- test$contingencyTable[[c(hallmarkGSC_index)]]
+  }
+  
+  GSAhyper_df_final <- bind_rows(GSAhyper_df_final, 
+                                 GSAhyper_df %>%
+                                   mutate(intersection = temp_name))
+  
+}
+
+GSAhyper_df_final$q_value <- p.adjust(GSAhyper_df_final$p_value)
+
+ggplot(GSAhyper_df_final %>%
+         filter(q_value <= 0.01) %>%
+         mutate(intersection = case_when(
+           intersection == "WT" ~ "WT Alone",
+           intersection == "Mock Transfected" ~ "Mock Trasnfected\nAlone",
+           TRUE ~ "WT /\nMock Transfected"
+         )) %>%
+         mutate(gene_set = stringr::str_remove(gene_set,
+                                               "HALLMARK_")),
+       aes(y = gene_set, x = intersection)) +
+  geom_point(aes(color = -log10(q_value),
+                 size = -log10(q_value),
+                 alpha = intersection)) +
+  viridis::scale_color_viridis(option = "D") +
+  scale_alpha_manual(values = c(1,1,0),
+                     breaks = c("WT Alone", "WT /\nMock Transfected", "Mock Trasnfected\nAlone")) +
+  scale_size_continuous(range = c(1,4)) +
+  xlab("DEG Intersection") +
+  ylab("Hallmark Gene Set") +
+  guides(size = "none",
+         alpha = "none") +
+  theme(text = element_text(size = 8),
+        axis.text.x = element_text(angle = 45, hjust = 1),
+        legend.key.height = unit(0.3, "cm"),
+        legend.key.width = unit(0.3, "cm"),
+        legend.position = "bottom",
+        legend.direction = "horizontal",
+        legend.margin = margin(t = -5, l = -100)) +
+  monocle3:::monocle_theme_opts()
+ggsave("SHP2_WT_mock_upregulated_intersection_GSEA.png",
+       dpi = 900, height = 3, width = 3)
+
+readr::write_csv(x = GSAhyper_df_final %>%
+                   filter(q_value <= 0.01) %>%
+                   mutate(intersection = case_when(
+                     intersection == "WT" ~ "WT Alone",
+                     TRUE ~ "WT /\nMock Transfected"
+                   )) %>%
+                   mutate(log10_FDR = -log10(q_value)),
+                 file = "SHP2_WT_mock_upregulated_intersection_GSEA_res_for_printing.csv",
+                 col_names = T)
+
+
+# ==============================================================================
+# Aggregate expression of union SHP2 mutant genes
+# ==============================================================================
+treatment_diff_test_SHP2 <- treatment_diff_test %>%
+  filter(grepl(pattern = "SHP2_mut", term)) %>%
+  filter(abs(normalized_effect) >= 0.05 & q_value <= 0.05) %>%
+  mutate(direction = case_when(
+    normalized_effect >= 0 ~ "Upregulated",
+    TRUE ~ "Downregulated"
+  ))
+
+DEG_union <- treatment_diff_test_SHP2 %>%
+  distinct(id) %>%
+  pull()
+
+colData(cds)$condition <- paste0(colData(cds)$timepoint,"_",
+                                 colData(cds)$SHP2_mut)
+
+cds_agg <- cds[DEG_union,
+               colData(cds)$EGF_dose %in% c(100, 250, 500, 1000)]
+
+cell_group_df <- tibble::tibble(cell=colData(cds_agg)$Cell, 
+                                cell_group=colData(cds_agg)$condition)
+
+agg_mat <- aggregate_gene_expression(cds_agg, 
+                                     gene_group_df = NULL,
+                                     cell_group_df = cell_group_df, 
+                                     norm_method = "log",
+                                     pseudocount = 1, 
+                                     scale_agg_values = T) %>%
+  as.data.frame() %>%
+  select(contains("24hr"))
+
+colnames(agg_mat) <- gsub("24hr_","",colnames(agg_mat))
+colnames(agg_mat) <- gsub("Q510E","Mock Transfected",colnames(agg_mat))
+
+agg_mat[agg_mat > 2] <- 2
+agg_mat[agg_mat < -2] <- -2
+
+hm <- ComplexHeatmap::Heatmap(matrix = agg_mat %>% as.matrix(), 
+                              name = "Z-Scored\nExpression",
+                              heatmap_legend_param = list(title = "Z-Scored\nExpression",
+                                                          title_gp = gpar(fontface = "bold", 
+                                                                          fontsize = 8),
+                                                          labels_gp = gpar(fontsize = 7),
+                                                          grid_width = unit(0.3, "cm"),
+                                                          legend_height = unit(1.8, "cm")),
+                              show_column_names = TRUE, 
+                              column_names_gp = gpar(fontsize = 8), 
+                              column_names_rot = 45,
+                              column_split = 6,
+                              row_split = 8,
+                              column_title = NULL,
+                              cluster_columns = TRUE, 
+                              cluster_rows = TRUE,
+                              row_title = "Gene", 
+                              row_title_gp = gpar(fontsize = 6),
+                              show_row_names = F,
+                              row_dend_width = unit(5,"mm"),
+                              column_dend_height = unit(5, "mm"),
+                              use_raster = F)
+draw(hm)
+
+hm <- ComplexHeatmap::Heatmap(matrix = agg_mat %>% as.matrix() %>% t(), 
+                              name = "Z-Scored\nExpression",
+                              heatmap_legend_param = list(title = "Z-Scored\nExpression",
+                                                          title_gp = gpar(fontface = "bold", 
+                                                                          fontsize = 8),
+                                                          labels_gp = gpar(fontsize = 7),
+                                                          grid_height = unit(0.3, "cm"),
+                                                          legend_width = unit(1.8, "cm"), 
+                                                          direction = "horizontal",
+                                                          title_position = "leftcenter"),
+                              show_row_names = TRUE, 
+                              row_names_gp = gpar(fontsize = 8), 
+                              row_split = 6,
+                              column_title = "Module %s",
+                              column_title_side = "bottom",
+                              column_title_rot = 45,
+                              column_split = 10,
+                              row_gap = unit(1.5, "mm"),
+                              cluster_columns = TRUE, 
+                              cluster_rows = TRUE,
+                              row_title = NULL, 
+                              column_title_gp = gpar(fontsize = 8),
+                              show_column_names = F,
+                              row_dend_width = unit(5,"mm"),
+                              column_dend_height = unit(5, "mm"),
+                              use_raster = F)
+draw(hm,
+     heatmap_legend_side = "bottom")
+
+pdf("SHP2_24hr_EGFhigh_aggregate_expression_DEG_union.pdf",width=6.5,height=3.5,compress = F)
+ht <- draw(hm,
+           heatmap_legend_side = "bottom"); col_clusters <- column_order(ht); row_clusters <- row_order(ht)
+dev.off()
+
+col_order_heatmap_mat <- data.frame("column" = colnames(agg_mat %>% as.matrix() %>% t())) %>%
+  rownames_to_column(var = "row_numb")
+col_order_heatmap_mat_final <- data.frame()
+for(name in 1:length(col_clusters)) {
+  col_order_heatmap_mat_temp <- col_order_heatmap_mat %>%
+    filter(row_numb %in% col_clusters[[name]]) %>%
+    mutate(`Gene Cluster` = name)
+  col_order_heatmap_mat_final <- bind_rows(col_order_heatmap_mat_final, 
+                                           col_order_heatmap_mat_temp)
+}
+
+col_order_heatmap_mat_final_named <- col_order_heatmap_mat_final %>%
+  select(-row_numb) %>%
+  dplyr::rename(id = column) %>%
+  left_join(rowData(cds) %>% as.data.frame() %>%
+              select(-num_cells_expressed),
+            by = c("id"))
+
+write_csv(x = col_order_heatmap_mat_final_named %>%
+            mutate(Module = `Gene Cluster`), 
+          file = "SHP2_24hr_EGFhigh_aggregate_expression_DEG_union_gene_modules.csv")
+
+gene_module_cluster_summary <- col_order_heatmap_mat_final_named %>%
+  group_by(`Gene Cluster`) %>%
+  summarise(count = n())
